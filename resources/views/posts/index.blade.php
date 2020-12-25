9a31c9110d0662d36ef4c1e0b36002738bb56e5a
Srdjan Rados <srdjan.rados90@gmail.com>
2020-12-25 13:15:11 +0100

    relations

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -4,11 +4,11 @@
     @foreach ($posts as $post)
         <p>
             {{-- <h3>{{ $post->unique }}</h3> --}}
-            <h3>
-                <a href="{{ route('posts.show', ['post' =>$post->id]) }}">{{ $post->unique }}</a>
-            </h3>
-                <a href="{{ route('posts.edit', ['post' => $post->id]) }}">Edit</a>
+            <h3><a href="{{ route('posts.show', ['post' =>$post->id]) }}">{{ $post->unique }}</a></h3>
 
+            <div class="mb-3">
+                <a class="btn btn-primary" href="{{ route('posts.edit', ['post' => $post->id]) }}">Edit</a>
+            </div>
         </p>
 
 
